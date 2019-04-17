14dc9e6eaf85edb4b4cf7615ee3a46507dc1b7cf
Manny Rodriguez <manny@theavatar.org>
2019-04-17 13:48:32 -0600

    enums for agent subrecords

---
+++ common/db/migrations/120_add_enum_values_for_agents_1.rb
@@ -0,0 +1,38 @@
+require_relative 'utils'
+
+Sequel.migration do
+
+  up do
+    $stderr.puts "Adding enumerations and values"
+
+    create_enum("maintenence_status", ["new", "upgraded", "revised_corrected", "derived", "deleted", "cancelled_obsolete", "deleted_split", "deleted_replaced"])
+
+		create_enum("publication_status", ["in_process", "approved"])
+
+		create_enum("romanization", ["int_std", "nat_std", "nl_assoc_std", "nl_bib_agency_std", "local_standard", "unknown_standard", "conv_rom_cat_agency", "not_applicable"])
+
+		create_enum("government_agency_type", ["ngo", "sac", "multilocal", "fed", "int_gov", "local", "multistate", "undetermined", "provincial", "unknown", "other", "natc"])
+
+		create_enum("reference_evaluation", ["tr_consistent", "tr_inconsistent", "not_applicable", "natc"])
+
+		create_enum("name_type", ["differentiated", "undifferentiated", "not_applicable", "natc"])
+
+		create_enum("level_of_detail", ["fully_established", "memorandum", "provisional", "preliminary", "not_applicable", "natc"])
+
+		create_enum("modified_record", ["not_modified", "shortened", "missing_characters", "natc"])
+
+		create_enum("cataloging_source", ["nat_bib_agency", "ccp", "other", "unknown", "natc"])
+
+		create_enum("source", ["naf", "snac", "local"], nil, true)
+
+		create_enum("identifier_type", ["loc", "lac", "local"], nil, true)
+
+		create_enum("agency_code_type", ["oclc", "local"], nil, true)
+
+		create_enum("xlink_actuate", ["onload", "onrequest", "other", "none"])
+
+		create_enum("maintenence_event_type", ["created", "cancelled", "deleted", "derived", "revised", "updated"])
+
+		create_enum("maintenence_agent_type", ["human", "machine"], "human")
+  end
+end
