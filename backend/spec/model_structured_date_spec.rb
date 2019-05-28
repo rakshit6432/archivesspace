e6e11d9a69f556062ab410ccfdd1758fdbbf5461
Manny Rodriguez <manny@theavatar.org>
2019-05-28 17:31:51 -0600

    renamed agent_dates to structured_dates; refactored dates so that they are split into separate tables; updated schemas, models and tests

---
+++ backend/spec/model_structured_date_spec.rb
@@ -0,0 +1,136 @@
+require_relative 'spec_helper'
+
+describe 'Structured Date model' do
+  it "label record is invalid without a date subrecord" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single")
+
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "single date labels should have one subrecord" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"}
+                                  ])
+
+    sdl.save
+  
+    expect(sdl.valid?).to eq(true)
+  end
+
+  it "ranged date labels should have two subrecords" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "range",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"},
+                                    {:date_role_enum => "end",
+                                    :date_expression => "Tomorrow"}
+                                  ])
+
+    sdl.save
+  
+    expect(sdl.valid?).to eq(true)
+  end
+
+  it "single date labels with more than one subrecords are invalid" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"},
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Tomorrow"}
+                                  ])
+
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "ranged date labels with less than two subrecords are invalid" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "range",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"}
+                                  ])
+
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "ranged date labels with more than two subrecords are invalid" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "range",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"},
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Tomorrow"},
+                                    {:date_role_enum => "end",
+                                    :date_expression => "Tomorrow"}
+                                  ])
+
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "ranged date labels should have a begin and an end subrecord" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "range",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"},
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Tomorrow"}
+                                  ])
+
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "is invalid unless a date is present in the subrecord" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin"}
+                                  ])
+  
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "is invalid unless a role is present in the subrecord" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single",
+                                  :structured_dates_attributes => [
+                                    {:date_expression => "begin"}
+                                  ])
+  
+    expect(sdl.valid?).to eq(false)
+  end
+
+  it "is valid if date expression is present in the subrecord" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_expression => "Yesterday"}
+                                  ])
+
+    sdl.save
+  
+    expect(sdl.valid?).to eq(true)
+  end
+
+  it "is valid if standardized date is present" do
+    sdl = StructuredDateLabel.new(:date_label => "other", 
+                                  :date_type_enum => "single",
+                                  :structured_dates_attributes => [
+                                    {:date_role_enum => "begin",
+                                    :date_standardized => Time.now }
+                                  ])
+
+    sdl.save
+  
+    expect(sdl.valid?).to eq(true)
+  end
+end
---
+++ common/schemas/structured_date.rb
@@ -0,0 +1,19 @@
+{
+  :schema => {
+    "$schema" => "http://www.archivesspace.org/archivesspace.json",
+    "version" => 1,
+    "type" => "object",
+
+    "properties" => {
+      "date_role_enum" => {"type" => "string", "dynamic_enum" => "date_role_enum" },
+
+      "date_expression" => {"type" => "string", "maxLength" => 255},
+      "date_standardized" => {"type" => "date-time", "maxLength" => 255},
+      "date_standardized_type_enum" => {"type" => "string", "dynamic_enum" => "begin_date_standardized_type_enum", "required" => "false"},
+  
+      "date_certainty" => {"type" => "string", "dynamic_enum" => "date_certainty", "required" => "false"},
+      "date_era" => {"type" => "string", "dynamic_enum" => "date_era", "required" => "false"},
+      "date_calendar" => {"type" => "string", "dynamic_enum" => "date_calendar", "required" => "false"}
+    },
+  },
+}
