f9c35b3d6b3ad40477776360fb2dad13dcb1cf85
Manny Rodriguez <manny@theavatar.org>
2019-05-29 16:31:57 -0600

    use formatted strings instead of DateTime for standardized_dates

--- backend/app/model/structured_date.rb
+++ backend/app/model/structured_date.rb
@@ -14,6 +14,14 @@ class StructuredDate < Sequel::Model(:structured_date)
     has_std_date = !self.date_standardized.nil? 
 
     errors.add(:base, "A date or date expression is required") unless has_expr_date || has_std_date
+
+    if has_std_date
+      matches_yyyy       = (self.date_standardized =~ /^[\d]{4}$/) == 0
+      matches_yyyy_mm    = (self.date_standardized =~ /^[\d]{4}-[\d]{2}$/) == 0
+      matches_yyyy_mm_dd = (self.date_standardized =~ /^[\d]{4}-[\d]{2}-[\d]{2}$/) == 0
+
+      errors.add(:base, "Standardized dates must be in YYYY, YYYY-MM, or YYYY-MM-DD format") unless matches_yyyy || matches_yyyy_mm || matches_yyyy_mm_dd
+    end
   end
 end
 
