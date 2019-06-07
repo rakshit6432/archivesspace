74675bcdbbbb10d51f9cacb31c684e5ba031aaba
Manny Rodriguez <manny@theavatar.org>
2019-06-07 14:53:20 -0600

    Add validation checking that all end dates come after all before dates

--- backend/app/model/structured_date_label.rb
+++ backend/app/model/structured_date_label.rb
@@ -21,14 +21,37 @@ class StructuredDateLabel < Sequel::Model(:structured_date_label)
   def validate
     count = self.structured_dates.length
 
-    has_begin = self.structured_dates.inject(false) {|carry, date| carry || date.date_role_enum == "begin"}
-    has_end = self.structured_dates.inject(false) {|carry, date| carry || date.date_role_enum == "end"}
+    begin_dates = self.structured_dates.select {|date| date.date_role_enum == "begin"}
+    end_dates = self.structured_dates.select {|date| date.date_role_enum == "end"}
+
+    has_begin = begin_dates.length > 0
+    has_end = end_dates.length > 0
 
     if self.date_type_enum == "single"
       errors.add(:base, "At lease one start date subrecord is required for single dates") unless has_begin
       errors.add(:base, "Single dates should not have end date subrecord") if has_end
     elsif self.date_type_enum == "range"
       errors.add(:base, "Ranged dates require begin and end dates") unless has_end && has_begin
+
+      # check that no standardized begin date is chronologically after any standardized end date
+      begin_dates.select! {|d| d.date_standardized}
+      end_dates.select!   {|d| d.date_standardized}
+
+      begin_dates.map! {|d| Time.parse(d.date_standardized)}
+      end_dates.map!   {|d| Time.parse(d.date_standardized)}
+
+      out_of_order = false
+      begin_dates.each do |bd|
+        end_dates.each do |ed|
+          out_of_order = bd > ed
+
+          break if out_of_order
+        end
+
+        break if out_of_order
+      end
+
+      errors.add(:base, "Begin dates must be later than end dates") if out_of_order
     end
   end 
 end
