8dfd4eaf69e9b72799d2b422175c8490f598cf64
Manny Rodriguez <manny@theavatar.org>
2019-06-17 15:25:41 -0600

    UI working for agent dates of existence

--- common/schemas/structured_date.rb
+++ common/schemas/structured_date.rb
@@ -5,7 +5,7 @@
     "type" => "object",
 
     "properties" => {
-      "date_role_enum" => {"type" => "string", "dynamic_enum" => "date_role_enum" },
+      "date_role_enum" => {"type" => "string", "dynamic_enum" => "date_role_enum", "ifmissing" => "error" },
 
       "date_expression" => {"type" => "string", "maxLength" => 255},
       "date_standardized" => {"type" => "string", "maxLength" => 255},
