dbcc49d307997775c4714e144ad314d0f6741770
Manny Rodriguez <manny@theavatar.org>
2019-09-25 17:13:05 -0600

    updating tests for language integration

--- backend/spec/model_agent_record_control_spec.rb
+++ backend/spec/model_agent_record_control_spec.rb
@@ -3,8 +3,7 @@ require_relative 'spec_helper'
 describe 'AgentRecordControl model' do
 
   it "allows agent_record_control records to be created" do
-    arc = AgentRecordControl.new(:language => "foo", 
-    														 :maintenance_status_enum => "new",
+    arc = AgentRecordControl.new(:maintenance_status_enum => "new",
                                  :publication_status_enum => "in_process",
                                  :romanization_enum => "int_std",
                                  :government_agency_type_enum => "ngo",
@@ -16,8 +15,6 @@ describe 'AgentRecordControl model' do
                                  :maintenance_agency => "maintenance_agency",
                                  :agency_name => "agency_name",
                                  :maintenance_agency_note => "maintenance_agency_note",
-                                 :script => "script",
-                                 :language_note => "language_note",
                                  :agent_person_id => rand(10000))
 
 
@@ -26,16 +23,14 @@ describe 'AgentRecordControl model' do
   end
 
   it "requires an agent_record_control to point to an agent record" do
-    arc = AgentRecordControl.new(:language => "foo", 
-                                 :maintenance_status_enum => "new")
+    arc = AgentRecordControl.new(:maintenance_status_enum => "new")
 
 
     expect(arc.valid?).to eq(false)
   end
 
   it "is invalid if an agent_record_control points to more than one agent record" do
-    arc = AgentRecordControl.new(:language => "foo", 
-                                 :maintenance_status_enum => "new",
+    arc = AgentRecordControl.new(:maintenance_status_enum => "new",
                                  :agent_person_id => rand(10000),
                                  :agent_family_id => rand(10000))
 
@@ -43,14 +38,12 @@ describe 'AgentRecordControl model' do
   end
 
   it "agent_record_control must point to unique agent record" do
-    a1  = AgentRecordControl.new(:language => "foo", 
-                                 :maintenance_status_enum => "new",
+    a1  = AgentRecordControl.new(:maintenance_status_enum => "new",
                                  :agent_person_id => 42)
 
     a1.save
 
-    a2  = AgentRecordControl.new(:language => "foo", 
-                                 :maintenance_status_enum => "new",
+    a2  = AgentRecordControl.new(:maintenance_status_enum => "new",
                                  :agent_person_id => 42)
 
     expect(a1.valid?).to eq(true)
