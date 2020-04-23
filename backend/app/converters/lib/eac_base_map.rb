9aaaf093e93d86176f89c577c58adb2a00f3eaa8
Manny Rodriguez <manny@theavatar.org>
2020-04-23 10:57:10 -0600

    work in progress: EAC imports

--- backend/app/converters/lib/eac_base_map.rb
+++ backend/app/converters/lib/eac_base_map.rb
@@ -1,12 +1,192 @@
-module EACBaseMap
+  module EACBaseMap
+
+  # These fields are common to all agent types and imported the same way
+  EAC_BASE_MAP_SUBFIELDS = {
+    "//eac-cpf//control/recordId" => {
+      :obj => :agent_record_identifier,
+      :rel => :agent_record_identifiers,
+      :map => {
+        "self::recordId" => Proc.new {|id, node|
+          val = node.inner_text
+          id[:record_identifier] = val
+          id[:primary_identifier] = true
+        }
+      },
+      :defaults => {
+        :primary_identifier => true,
+        :source_enum => "local" 
+      }
+    },
+    "//eac-cpf//control/otherRecordId" => {
+      :obj => :agent_record_identifier,
+      :rel => :agent_record_identifiers,
+      :map => {
+         "self::otherRecordId" => Proc.new {|id, node|
+          val = node.inner_text
+          id[:record_identifier] = val
+          id[:primary_identifier] = false
+          id[:identifier_type_enum] = node.attr("localType")
+        },
+      },
+      :defaults => {
+        :primary_identifier => false,
+        :source_enum => "local" 
+      }
+    },
+    "//eac-cpf//control" => {
+      :obj => :agent_record_control,
+      :rel => :agent_record_controls,
+      :map => {
+        "descendant::maintenanceStatus" => Proc.new {|arc, node|
+          val = node.inner_text
+          arc[:maintenance_status_enum] = val
+        },
+        "descendant::maintenanceAgency/agencyCode" => Proc.new {|arc, node|
+          val = node.inner_text
+          arc[:maintenance_agency] = val
+        },
+        "descendant::maintenanceAgency/agencyName" => Proc.new {|arc, node|
+          val = node.inner_text
+          arc[:agency_name] = val
+        },
+        "descendant::maintenanceAgency/descriptiveNote" => Proc.new {|arc, node|
+          val = node.inner_text
+          arc[:maintenance_agency_note] = val
+        },
+        "descendant::publicationStatus" => Proc.new {|arc, node|
+          val = node.inner_text
+          arc[:publication_status_enum] = val
+        },
+        "descendant::languageDeclaration/language" => Proc.new {|arc, node|
+          arc[:language] = node.attr("languageCode")
+          arc[:script] = node.attr("scriptCode")
+        },
+        "descendant::languageDeclaration/descriptiveNote" => Proc.new {|arc, node|
+          val = node.inner_text
+          arc[:language_note] = val
+        }
+      },
+      :defaults => {
+        :maintenance_status_enum => "new"
+      }
+    },
+    "//eac-cpf/control/conventionDeclaration" => {
+      :obj => :agent_conventions_declaration,
+      :rel => :agent_conventions_declarations,
+      :map => {
+        "descendant::abbreviation" => Proc.new {|dec, node|
+          val = node.inner_text.downcase
+          dec[:name_rule] =  val
+        },
+        "descendant::citation" => Proc.new {|dec, node|
+          val = node.inner_text
+          dec[:citation] = (val.nil? || val.length == 0) ? "citation" : val
+          dec[:file_uri] = node.attr("href")
+          dec[:file_version_xlink_actuate_attribute] = node.attr("actuate")
+          dec[:file_version_xlink_show_attribute] = node.attr("show")
+          dec[:xlink_title_attribute] = node.attr("title")
+          dec[:xlink_role_attribute] = node.attr("role")
+          dec[:last_verified_date] = node.attr("lastDateTimeVerified")
+        },
+        "descendant::descriptiveNote" => Proc.new {|dec, node|
+          val = node.inner_text
+          dec[:descriptive_note] = val
+        },
+      },
+      :defaults => {
+        :citation => "citation",
+        :name_rule => "local"
+      }
+    },
+    "//eac-cpf/control/maintenanceHistory/maintenanceEvent" => {
+      :obj => :agent_maintenance_history,
+      :rel => :agent_maintenance_histories,
+      :map => {
+        "descendant::eventType" => Proc.new {|me, node|
+          val = node.inner_text.downcase
+          me[:maintenance_event_type_enum] =  val
+        },
+        "descendant::agentType" => Proc.new {|me, node|
+          val = node.inner_text
+          me[:maintenance_agent_type_enum] =  val
+        },
+        "descendant::agent" => Proc.new {|me, node|
+          val = node.inner_text
+          me[:agent] =  val
+        },
+        "descendant::eventDateTime" => Proc.new {|me, node|
+          val = node.attr("standardDateTime")
+          val2 = node.inner_text
+
+          me[:event_date] =  val
+          me[:event_date] = val2 if val.nil?
+        },
+        "descendant::eventDescription" => Proc.new {|me, node|
+          val = node.inner_text
+          me[:descriptive_note] =  val
+        },
+      },
+      :defaults => {
+      }
+    },
+    "//eac-cpf/control/sources/source" => {
+      :obj => :agent_sources,
+      :rel => :agent_sources,
+      :map => {
+        "self::source" => Proc.new {|s, node|
+          s[:file_uri] = node.attr("href")
+          s[:file_version_xlink_actuate_attribute] = node.attr("actuate")
+          s[:file_version_xlink_show_attribute] = node.attr("show")
+          s[:xlink_title_attribute] = node.attr("title")
+          s[:xlink_role_attribute] = node.attr("role")
+          s[:last_verified_date] = node.attr("lastDateTimeVerified")
+        },
+        "descendant::sourceEntry" => Proc.new {|s, node|
+          val = node.inner_text
+          s[:source_entry] = val
+        },
+        "descendant::descriptiveNote" => Proc.new {|s, node|
+          val = node.inner_text
+          s[:descriptive_note] = val
+        },
+      },
+      :defaults => {
+      }
+    },
+    "//eac-cpf/cpfDescription/identity/entityId" => {
+      :obj => :agent_identifier,
+      :rel => :agent_identifiers,
+      :map => {
+        "self::entityId" => Proc.new {|id, node|
+          val = node.inner_text
+          id[:entity_identifier] = val
+          id[:identifier_type_enum] = node.attr("localType")
+        }
+      },
+      :defaults => {
+      }
+    },
+    "//eac-cpf//biogHist" => {
+      :obj => :note_bioghist,
+      :rel => :notes,
+      :map => {
+        "self::biogHist" => Proc.new {|note, node|
+          note['subnotes'] << {
+            'jsonmodel_type' => 'note_text',
+            'content' => node.inner_text
+          }
+        }
+      },
+      :defaults => {
+        :label => 'default label'
+      }
+    }
+  }
 
   def EAC_BASE_MAP
     {
-      # RECORD CONTROL METADATA
-
       # AGENT PERSON
-      # //cpfDescription[child::identity/child::entityType='person']
-      "//eac-cpf" => {
+      "//eac-cpf//cpfDescription[child::identity/child::entityType='person']" => {
         :obj => :agent_person,
         :map => {
           # NAMES (PERSON)
@@ -14,7 +194,53 @@ module EACBaseMap
             :obj => :name_person,
             :rel => :names,
             :map => {
-              "descendant::part" => Proc.new {|name, node|
+              "descendant::part[@localType='prefix']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:prefix] = val
+              },
+              "descendant::part[@localType='suffix']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:suffix] = val
+              },
+              "descendant::part[@localType='title']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:title] = val
+              },
+              "descendant::part[@localType='surname']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:primary_name] = val
+              },
+              "descendant::part[@localType='forename']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:rest_of_name] = val
+              },
+              "descendant::part[@localType='numeration']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:number] = val
+              },
+              "descendant::part[@localType='fuller_form']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:fuller_form] = val
+              },
+              "descendant::part[@localType='dates']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:dates] = val
+              },
+              "descendant::part[@localType='qualifier']" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:qualifier] = val
+              },
+              "descendant::authorizedForm" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:source] = val
+                name[:authorized] = true
+              },
+              "descendant::alternativeForm" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:source] = val
+                name[:authorized] = false
+              },
+              "descendant::part[@localType='']" => Proc.new {|name, node|
                 val = node.inner_text
                 name[:primary_name] = val
                 name[:dates] = val.scan(/[0-9]{4}-[0-9]{4}/).flatten[0]
@@ -26,63 +252,77 @@ module EACBaseMap
               :primary_name => 'primary name',
               :name_order => 'direct',
             }
-          },
-          "descendant::control" => {
-            :obj => :agent_record_identifier,
-            :rel => :agent_record_identifiers,
+          }
+        }.merge(EAC_BASE_MAP_SUBFIELDS)
+      },
+      # AGENT CORPORATE ENTITY
+      "//eac-cpf//cpfDescription[child::identity/child::entityType='corporateBody']" => {
+        :obj => :agent_corporate_entity,
+        :map => {
+          # NAMES (PERSON)
+          "descendant::nameEntry" => {
+            :obj => :name_corporate_entity,
+            :rel => :names,
             :map => {
-              "descendant::recordId" => Proc.new {|id, node|
+              "descendant::part" => Proc.new {|name, node|
                 val = node.inner_text
-                id[:record_identifier] = val
+                name[:primary_name] = val
+                name[:dates] = val.scan(/[0-9]{4}-[0-9]{4}/).flatten[0]
               },
-            },
-              :defaults => {
-                :primary_identifier => false,
-                :source_enum => "naf" 
-              }
-            },
-          "descendant::conventionDeclaration" => {
-            :obj => :agent_conventions_declaration,
-            :rel => :agent_conventions_declarations,
-            :map => {
-              "descendant::abbreviation" => Proc.new {|dec, node|
-                val = node.inner_text.downcase
-                dec[:name_rule] =  val
-              },
-              "descendant::citation" => Proc.new {|dec, node|
+              "descendant::authorizedForm" => Proc.new {|name, node|
                 val = node.inner_text
-                dec[:citation] = val
+                name[:source] = val
+                name[:authorized] = true
               },
-              "descendant::descriptiveNote" => Proc.new {|dec, node|
+              "descendant::alternativeForm" => Proc.new {|name, node|
                 val = node.inner_text
-                dec[:descriptive_note] = val
+                name[:source] = val
+                name[:authorized] = false
               },
             },
             :defaults => {
-              :citation => "citation",
-              :name_rule => "local", 
-              :file_version_xlink_actuate_attribute => "none",
-              :file_version_xlink_show_attribute => "none"
-            }
-          },
-          "descendant::biogHist" => {
-            :obj => :note_bioghist,
-            :rel => :notes,
-            :map => {
-              "self::biogHist" => Proc.new {|note, node|
-                note['subnotes'] << {
-                  'jsonmodel_type' => 'note_text',
-                  'content' => node.inner_text
-                }
-              }
-            },
-            :defaults => {
-              :label => 'default label'
+              :source => 'local',
+              :rules => 'local',
+              :primary_name => 'primary name',
+              :name_order => 'direct',
             }
           }
-        }
+        }.merge(EAC_BASE_MAP_SUBFIELDS)
+      },
+      # AGENT FAMILY
+      "//eac-cpf//cpfDescription[child::identity/child::entityType='family']" => {
+        :obj => :agent_family,
+        :map => {
+          # NAMES (PERSON)
+          "descendant::nameEntry" => {
+            :obj => :name_family,
+            :rel => :names,
+            :map => {
+              "descendant::part" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:family_name] = val
+                name[:dates] = val.scan(/[0-9]{4}-[0-9]{4}/).flatten[0]
+              },
+              "descendant::authorizedForm" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:source] = val
+                name[:authorized] = true
+              },
+              "descendant::alternativeForm" => Proc.new {|name, node|
+                val = node.inner_text
+                name[:source] = val
+                name[:authorized] = false
+              },
+            },
+            :defaults => {
+              :source => 'local',
+              :rules => 'local',
+              :primary_name => 'primary name',
+              :name_order => 'direct',
+            }
+          }
+        }.merge(EAC_BASE_MAP_SUBFIELDS)
       }
-
     }
   end
 end
--- backend/spec/lib_eac_converter_spec.rb
+++ backend/spec/lib_eac_converter_spec.rb
@@ -9,16 +9,150 @@ describe 'EAC converter' do
     EACConverter
   }
 
-  let(:test_file) {
+  let(:person_agent_1) {
     File.expand_path("../app/exporters/examples/eac/feynman-richard-phillips-1918-1988-cr.xml",
                      File.dirname(__FILE__))
   }
 
+  let(:person_agent_2) {
+    File.expand_path("../app/exporters/examples/eac/MMeT-C_2012_RCR00751.xml",
+                     File.dirname(__FILE__))
+  }
 
-  it "did something" do
-    record = convert(test_file).first
+  let(:person_agent_3) {
+    File.expand_path("../app/exporters/examples/eac/xmleac.xml",
+                     File.dirname(__FILE__))
+  }
 
-    expect(record).not_to be_nil
-    expect(record['names'][0]['primary_name']).to eq("Feynman, Richard Phillips, 1918-1988.")
+
+  describe "people agents" do
+    it "imports primary_name" do
+      record = convert(person_agent_1).first
+
+      expect(record).not_to be_nil
+      expect(record['names'][0]['primary_name']).to eq("Feynman, Richard Phillips, 1918-1988.")
+    end
+
+    it "imports bioghist notes" do
+      record = convert(person_agent_2).first
+      note = record["notes"].first["subnotes"].first
+
+
+      expect(record).not_to be_nil
+      expect(note).not_to be_nil
+
+      expect(note["content"]).to match(/Richard H. Lufkin was a shoe machine engineer/)
+    end
+
+    it "imports recordId as primary agent_record_identifier" do
+      record = convert(person_agent_2).first
+
+      ari = record["agent_record_identifiers"].first
+
+
+      expect(ari).not_to be_nil
+
+      expect(ari["record_identifier"]).to eq("RCR00751")
+      expect(ari["primary_identifier"]).to eq(true)
+    end
+
+    it "imports otherRecordId as not primary agent_record_identifier" do
+      record = convert(person_agent_3).first
+
+      ars = record["agent_record_identifiers"]
+
+      expect(ars.length).to eq(8)
+      expect(ars[1]["record_identifier"]).to eq("11850391X")
+      expect(ars[1]["primary_identifier"]).to eq(false)
+      expect(ars[1]["identifier_type_enum"]).to eq("PPN")
+
+      expect(ars[2]["record_identifier"]).to eq("http://kalliope-verbund.info/gnd/11850391X")
+      expect(ars[2]["primary_identifier"]).to eq(false)
+      expect(ars[2]["identifier_type_enum"]).to eq("uriKPE")
+    end
+
+    it "imports agent_record_control tags" do
+      record = convert(person_agent_3).first
+
+      arc = record["agent_record_controls"].first
+
+      expect(arc["maintenance_status_enum"]).to eq("new")
+      expect(arc["agency_name"]).to eq("Deutsche Nationalbibliothek")
+      expect(arc["maintenance_agency"]).to eq("DE-101")
+      expect(arc["maintenance_agency_note"]).to eq("Agency Note")
+      expect(arc["publication_status_enum"]).to eq("approved")
+      expect(arc["language"]).to eq("ger")
+      expect(arc["script"]).to eq("Latn")
+      expect(arc["language_note"]).to eq("Language Note")
+    end
+
+    it "imports agent_conventions_declaration tags" do
+      record = convert(person_agent_3).first
+
+      acd = record["agent_conventions_declarations"].first
+
+      expect(acd["name_rule"]).to eq("rda")
+      expect(acd["citation"]).to eq("The Citation")
+      expect(acd["file_uri"]).to eq("http://www.google.com")
+      expect(acd["file_version_xlink_actuate_attribute"]).to eq("onRequest")
+      expect(acd["file_version_xlink_show_attribute"]).to eq("new")
+      expect(acd["xlink_title_attribute"]).to eq("xlink title")
+      expect(acd["xlink_role_attribute"]).to eq("xlink role")
+      expect(acd["descriptive_note"]).to eq("Convention Note")
+      expect(acd["last_verified_date"]).to eq("2000-07-01")
+    end
+
+    it "imports agent_maintenance_history tags" do
+      record = convert(person_agent_3).first
+
+      mh = record["agent_maintenance_histories"].first
+
+      expect(record["agent_maintenance_histories"].length).to eq(2)
+
+      expect(mh["maintenance_event_type_enum"]).to eq("created")
+      expect(mh["maintenance_agent_type_enum"]).to eq("human")
+      expect(mh["agent"]).to eq("W4")
+      expect(mh["event_date"]).to eq("1988-07-01")
+      expect(mh["descriptive_note"]).to eq("Event note 1")
+    end
+
+    it "imports agent_source tags" do
+      record = convert(person_agent_3).first
+
+      s = record["agent_sources"].first
+
+      expect(s["source_entry"]).to eq("Pressearchiv des Herder-Instituts Marburg")
+      expect(s["file_uri"]).to eq("http://www.googlew.com")
+      expect(s["file_version_xlink_actuate_attribute"]).to eq("onRequest")
+      expect(s["file_version_xlink_show_attribute"]).to eq("new")
+      expect(s["xlink_title_attribute"]).to eq("xlink title")
+      expect(s["xlink_role_attribute"]).to eq("xlink role")
+      expect(s["descriptive_note"]).to eq("Source Note")
+      expect(s["last_verified_date"]).to eq("2001-07-01")
+    end
+
+    it "imports agent_identifier tags" do
+      record = convert(person_agent_3).first
+
+      id = record["agent_identifiers"].first
+
+      expect(id["entity_identifier"]).to eq("auto gen number")
+      expect(id["identifier_type_enum"]).to eq("PPX")
+    end
+
+    it "imports other parts of name" do
+      record = convert(person_agent_3).first
+
+      expect(record['names'][0]['title']).to eq("Dr.")
+      expect(record['names'][0]['prefix']).to eq("Mrs.")
+      expect(record['names'][0]['primary_name']).to eq("Arendt")
+      expect(record['names'][0]['rest_of_name']).to eq("Hannah")
+      expect(record['names'][0]['suffix']).to eq("IV")
+      expect(record['names'][0]['number']).to eq("4")
+      expect(record['names'][0]['fuller_form']).to eq("Fuller Form")
+      expect(record['names'][0]['dates']).to eq("1906-1975")
+      expect(record['names'][0]['qualifier']).to eq("qualifier")
+
+    end
   end
 end
